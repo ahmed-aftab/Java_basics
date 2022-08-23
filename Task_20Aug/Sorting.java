fe4b4ca35c7ada00a8e0be318a18954e7e50d5eb
Ahmed Aftab <ahmed.aftab@emumba.com>
2022-08-24 00:10:08 +0500

    changes saved

--- Task_20Aug/Sorting.java
+++ Task_20Aug/Sorting.java
@@ -7,7 +7,11 @@ public class Sorting {
     public static void main(String args[]){
         int[] Array=GetArray();
         int[] SortedArray=SortArray(Array);
+//        int[] SortedArray2=SortArray2(Array);
+        System.out.println("Sorted Array 1: ");
         PrintArray(SortedArray);
+//        System.out.println("Sorted Array 2: ");
+//        PrintArray(SortedArray2);
     }
     public static int[] GetArray(){
         System.out.println("Please enter the size of array: ");
@@ -20,26 +24,43 @@ public class Sorting {
         }
         return arr;
     }
-    public static int[] SortArray(int[] array){
-        int[] arr = new int[array.length];
-        int num=0;
-        for(int z=0; z< array.length;z++ ) {
-            int y=0;
-            for (int x = 0; x < array.length; x++) {
-                if (array[x] > y) {
-                    y = array[x];
-                    num=x;
+    public static int[] SortArray(int[] array) {
+        int num = 0;
+        for (int j = 0; j <= 10; j++){
+            for (int z = 0; z < (array.length - 1); z++) {
+                if (array[z + 1] < array[z]) {
+                    num = array[z];
+                    array[z] = array[z + 1];
+                    array[z + 1] = num;
                 }
             }
-            array[num]=0;
-            arr[z]=y;
         }
-        return arr;
+        return array;
     }
     public static void PrintArray(int[] array){
         for(int x=0;x< array.length;x++){
             System.out.print(array[x]+"|");
         }
+    }
+    public static int[] SortArray2(int[] array) {
+        int x=0;
+        for (int j = 0; j < array.length; j++){
+            int num = array[j];
+            for (int z = j+1; z < (array.length); z++) {
+                if (array[z] < num) {
+                    num = array[z];
+                    x = z;
+                }
+
+            }
+                    array[x]=array[j];
+                    array[j] = num;
+
+
+
+
+        }
+        return array;
     }
 
 }
