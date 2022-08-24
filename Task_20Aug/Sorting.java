fd81bf5a355f4ec6720b5457fd852834824713ad
Ahmed Aftab <ahmed.aftab@emumba.com>
2022-08-24 09:53:06 +0500

    Changes made

--- Task_20Aug/Sorting.java
+++ Task_20Aug/Sorting.java
@@ -1,5 +1,4 @@
 package Task_20Aug;
-
 import java.lang.reflect.Array;
 import java.util.Scanner;
 // Program to sort an integer array in java
@@ -7,11 +6,8 @@ public class Sorting {
     public static void main(String args[]){
         int[] Array=GetArray();
         int[] SortedArray=SortArray(Array);
-//        int[] SortedArray2=SortArray2(Array);
-        System.out.println("Sorted Array 1: ");
+        System.out.println("Sorted Array : ");
         PrintArray(SortedArray);
-//        System.out.println("Sorted Array 2: ");
-//        PrintArray(SortedArray2);
     }
     public static int[] GetArray(){
         System.out.println("Please enter the size of array: ");
@@ -24,25 +20,12 @@ public class Sorting {
         }
         return arr;
     }
-    public static int[] SortArray(int[] array) {
-        int num = 0;
-        for (int j = 0; j <= 10; j++){
-            for (int z = 0; z < (array.length - 1); z++) {
-                if (array[z + 1] < array[z]) {
-                    num = array[z];
-                    array[z] = array[z + 1];
-                    array[z + 1] = num;
-                }
-            }
-        }
-        return array;
-    }
     public static void PrintArray(int[] array){
         for(int x=0;x< array.length;x++){
             System.out.print(array[x]+"|");
         }
     }
-    public static int[] SortArray2(int[] array) {
+    public static int[] SortArray(int[] array) {
         int x=0;
         for (int j = 0; j < array.length; j++){
             int num = array[j];
@@ -51,14 +34,9 @@ public class Sorting {
                     num = array[z];
                     x = z;
                 }
-
             }
                     array[x]=array[j];
                     array[j] = num;
-
-
-
-
         }
         return array;
     }
